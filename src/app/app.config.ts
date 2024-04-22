0a1c2cc700801625325dfd8b34023d71d380fc5e
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-04-22 21:30:38 +0200

    ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { BookReducer } from './books/book.reducer';
+import { provideStore, StoreModule } from '@ngrx/store';
+import { AppState } from './app.state';
+import { BookEffects } from './books/book.effects';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      StoreModule.forRoot<AppState>({
+        book: BookReducer,
+      }),
+      EffectsModule.forRoot([BookEffects]),
+      StoreDevtoolsModule.instrument()
+    ),
+  ],
 };
